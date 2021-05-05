be5941eb0a22d26825d6e366eff535ab1fde8cd6
hieubui09 <hieubdph09616@fpt.edu.vn>
2021-05-05 17:51:43 +0700

    5-5

--- src/layout/main.svelte
+++ src/layout/main.svelte
@@ -1,26 +1,30 @@
 <script>
-    let title = "hieu duc bui";
+  
 </script>
 
 <main>
     <section>
         <div class="Lighthouse">
             <div class="Lighthouse__text">
-                <h3>Nền tảng tài chính thông minh hàng đầu trên thế giới</h3>
-                <p>Tham gia Bitkingreturns của chúng tôi để xây dựng một
-                 nền kinh tế phi tập trung hoàn toàn minh bạch</p>
-                 <button>Tham Gia</button>
-                 <p>Tìm hieur thêm</p>
+                <h1>Nền tảng tài chính thông minh hàng đầu trên thế giới</h1>
+                <p>
+                    Tham gia Bitkingreturns của chúng tôi để xây dựng <br /> một
+                    nền kinh tế phi tập trung hoàn toàn minh bạch
+                </p>
+                <div>
+                    <button>Tham Gia</button>
+                    <p>Tìm hiểu thêm &#10141;</p>
+                </div>
             </div>
-            <!-- <div class="Lighthouse__img">
-                <img src="images/Group 40.png" alt="" />
-            </div> -->
+            <div class="Lighthouse__img">
+                <img src="images/Group B.png" alt="" />
+            </div>
         </div>
         <div class="wrapper">
             <div class="trademark">
                 <div><img src="images/bcio.png" alt="" /></div>
                 <div><img src="images/Group.png" alt="" /></div>
-                <div><img src="images/Group01.png" alt="" /></div>
+                <div><img src="images/Groupgalaxy.png" alt="" /></div>
                 <div><img src="images/Vector01.png" alt="" /></div>
                 <div><img src="images/Techkon_logo.png" alt="" /></div>
             </div>
@@ -55,11 +59,9 @@
                 </div>
             </div>
             <div class="content__dautu">
-                <div class="dautu__img">
-                    <img src="images/dautu.png" alt="" />
-                </div>
-                <div class="dautu__content">
-                    <h3>Mã thông báo trên TRON tốt nhất</h3>
+                <div class="dautu__text">
+                    <h3>Mua BTR</h3>
+                    <h2>Mã thông báo trên TRON tốt nhất</h2>
                     <p>
                         Chúng tôi sử dụng TRON như một công nghệ mới và xây dựng
                         BTR trên nền tảng hợp đồng thông minh. TRON giúp cung
@@ -67,67 +69,81 @@
                         trên thế giới, trong đó bao gồm BTR – hứa hẹn sẽ phát
                         triển mạnh mẽ trong năm 2021
                     </p>
-                    <button>Tham Gia</button>
+                    <button>Tham Gia &#10141;</button>
                 </div>
             </div>
             <div class="title">
                 <h2>Lợi ích của <br /> Bitkingreturns</h2>
             </div>
             <div class="grid-container">
-                <div class="item1">
-                    <img src="images/01.png" alt="" />
-                    <h3>
-                        Chia sẻ giúp bạn tạo ra một khoản lợi nhuận lớn mỗi ngày
-                    </h3>
-                    <p>
-                        Bạn sẽ nhận được nhiều hoa hồng mỗi ngày thông qua số
-                        lượng vé mua mà bạn đã giới thiệu. Nền tảng
-                        Bitkingreturns hoàn toàn tự động rút tiền và nạp tiền.
-                    </p>
+                <div class="item1__grid">
+                    <div class="item1__text">
+                        <img src="images/01.png" alt="" />
+                        <h3>
+                            Chia sẻ giúp bạn tạo ra một khoản lợi nhuận lớn mỗi
+                            ngày
+                        </h3>
+                        <p>
+                            Bạn sẽ nhận được nhiều hoa hồng mỗi ngày thông qua
+                            số lượng vé mua mà bạn đã giới thiệu. Nền tảng
+                            Bitkingreturns hoàn toàn tự động rút tiền và nạp
+                            tiền.
+                        </p>
+                        <p>Read more &#10141;</p>
+                    </div>
                 </div>
-                <div class="item2">
-                    <img src="images/Asset 001.png" alt="" />
+                <div class="item2__grid">
+                    <div class="item2__text">
+                        <img src="images/02.png" alt="" />
+                        <h3>Hệ thống bảo vệ tài sản nhiều tầng</h3>
+                        <p>
+                            Chúng tôi đã xây dựng hệ thống bảo vệ chắc chắn và
+                            chi tiết để ngăn chặn những tin tặc, những kẻ lừa
+                            đảo tấn công và lấy cắp tài sản của bạn
+                        </p>
+                        <p>Read more &#10141;</p>
+                    </div>
                 </div>
-                <div class="item3">
-                    <img src="images/Asset 002.png" alt="" />
+                <div class="item3__grid">
+                    <div class="item3__text">
+                        <img src="images/03.png" alt="" />
+                        <h3>Kiếm tiền trở nên dễ dàng với Bitkingreturns</h3>
+                        <p>
+                            Bạn đang tìm kiếm một nền tảng tài chính an toàn,
+                            đáng tin cậy và tạo ra nhiều lợi nhuận trong tương
+                            lai? Vậy thì Bitkingreturns - nền tảng được xây dựng
+                            trên công trình nghiên cứu của hơn 30 giáo sư kinh
+                            tế hàng đầu trên thế giới - chính là giải pháp hoàn
+                            hảo cho bạn.
+                        </p>
+                        <p>Read more &#10141;</p>
+                    </div>
                 </div>
-                <div class="item4">
-                    <img src="images/02.png" alt="" />
-                    <h3>Hệ thống bảo vệ tài sản nhiều tầng</h3>
-                    <p>
-                        Chúng tôi đã xây dựng hệ thống bảo vệ chắc chắn và chi
-                        tiết để ngăn chặn những tin tặc, những kẻ lừa đảo tấn
-                        công và lấy cắp tài sản của bạn
-                    </p>
-                </div>
-                <div class="item5">
-                    <img src="images/03.png" alt="" />
-                    <h3>Kiếm tiền trở nên dễ dàng với Bitkingreturns</h3>
-                    <p>
-                        Bạn đang tìm kiếm một nền tảng tài chính an toàn, đáng
-                        tin cậy và tạo ra nhiều lợi nhuận trong tương lai? Vậy
-                        thì Bitkingreturns - nền tảng được xây dựng trên công
-                        trình nghiên cứu của hơn 30 giáo sư kinh tế hàng đầu
-                        trên thế giới - chính là giải pháp hoàn hảo cho bạn.
-                    </p>
-                </div>
-                <div class="item6">
-                    <img src="images/Asset 003.png" alt="" />
-                </div>
-                <div class="item7" />
-                <div class="item8" />
             </div>
+        </div>
+        <div class="space" >  
+        <h3></h3>
         </div>
         <div class="content_kiemtien">
             <div class="slider">
                 <div>
                     <img src="images/Group 32.png" alt="" />
                 </div>
+
+                <div class="putton--btn">
+                    <div class="prev" >
+                        <img src="images/one1.png" alt="">
+                    </div>
+                    <div class="next"> 
+                        <img src="images/one2.png" alt="">
+                    </div>
+                </div>
+                
             </div>
             <div class="kiemtien_title">
                 <p>Làm sao để bắt đầu?</p>
                 <h3>Bắt đầu kiếm tiền Với những bước đơn giản</h3>
-                <p>
+                <p class="p">
                     Bắt đầu với những bước đơn giản: Đầu tư ngay trong tầm bay
                     bạn từ những bước click chuột đơn giản
                 </p>
@@ -135,7 +151,8 @@
         </div>
         <div class="wrapper">
             <div class="content_laptop">
-                <div class="laptop_tiile">
+                <div class="laptop_title">
+                    <h2>Responsible</h2>
                     <h3>Tương thích với tất cả các loại màn hình</h3>
                     <p>
                         Trang web của chúng tôi được nâng cấp và có thể tương
@@ -144,9 +161,7 @@
                         các nền tảng khác.
                     </p>
                 </div>
-                <div class="laptop_img">
-                    <img src="images/112 1.png" alt="" />
-                </div>
+                
             </div>
             <div class="title">
                 <h2>
@@ -154,27 +169,27 @@
                     trên thế giới
                 </h2>
             </div>
-            <div class="content_world">
-                <div class="flex">
-                    <div><img src="images/Vector_user.png" alt="" /></div>
-                    <div>
-                        <h1>34523</h1>
-                        <p>Người dùng</p>
-                    </div>
+        </div>
+        <div class="content_world">
+            <div class="flex">
+                <div><img src="images/Vector_user.png" alt="" /></div>
+                <div class="flex_text">
+                    <h1>34523</h1>
+                    <p>Người dùng</p>
                 </div>
-                <div  class="flex">
-                    <div><img src="images/Vector_011.png" alt="" /></div>
-                    <div>
-                        <h1>34523</h1>
-                        <p>Vé trung bình mỗi ngày</p>
-                    </div>
+            </div>
+            <div class="flex">
+                <div><img src="images/Vector_011.png" alt="" /></div>
+                <div class="flex_text">
+                    <h1>34523</h1>
+                    <p>Vé trung bình mỗi ngày</p>
                 </div>
-                <div  class="flex">
-                    <div><img src="images/Vector_002.png" alt="" /></div>
-                    <div>
-                        <h1>34523</h1>
-                        <p>Người dùng chiến thắng</p>
-                    </div>
+            </div>
+            <div class="flex">
+                <div><img src="images/Vector_002.png" alt="" /></div>
+                <div class="flex_text">
+                    <h1>34523</h1>
+                    <p>Người dùng chiến thắng</p>
                 </div>
             </div>
         </div>
@@ -182,7 +197,6 @@
             <h1>Kiếm tiền thật dễ dàng trên nền tảng Bitkingreturns</h1>
             <button class="now">Tham Gia ngay &#10140;</button>
             <button class="add">Tìm hiểu thêm &#10140;</button>
-            <!-- <img src="images/Rectangle 17.png" alt="" /> -->
         </div>
     </section>
 </main>
