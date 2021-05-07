e865b7e817788a22842418c2cb1e156d2a0189ff
hieubui09 <hieubdph09616@fpt.edu.vn>
2021-05-08 02:06:33 +0700

    02h-8-5

--- src/layout/footer.svelte
+++ src/layout/footer.svelte
@@ -3,35 +3,68 @@
 </script>
 <main>
    <footer>
-       <div class="ft__img">
-           <a href=""><img src="images/logo_bitking.png" alt=""></a>
-       </div>
        <div class="ft__row">
-           <div class="fl__text">
-            <br>
-            <p>Support@bitkingreturns.com</p>
+            <div class="ft__one">
+                <div class="fl__one__desktop">
+                    <div class="ft__img">
+                        <a href=""><img src="images/logo_bitking.png" alt="">
+                        </a>
+                    </div>
+                    <p>Support@bitkingreturns.com</p>
+                </div>
+                <div class="fl__one__mobi">
+                    <div class="ft__img">
+                        <a href=""><img src="images/logo_bitking.png" alt=""></a>
+                    </div>
+                    <p>Why I say old chap that is sping lavatory chip shop gosh off his, smashing boot are you taking the piss posh loo brilliant matie boy young.</p>
+                </div>
             </div>
-           <div class="about">
-               <h2>About Us</h2>
-            <ul> 
-                <li><a href="">Giới thiệu</a></li>
-                <li><a href="">Lợi ích</a></li>
-                <li><a href="">Buy BKT</a></li>
-                <li><a href="">Hướng dẫn</a></li>
-                <li><a href="">Liện hệ</a></li>
-              </ul>
-              
-           </div>
-           <div class="tutorial">
-            <h2>Hướng Dẫn</h2>
-            <ul> 
-                <li><a href="#home">Rút tiền</a></li>
-                <li><a href="#news">Nạp tiền</a></li>
-                <li><a href="#contact">Lấy mã </a></li>
-                <li><a href="#about">Chuyển tiền</a></li>
-                <li><a href="#about">Kiểm tra tickit</a></li>
-              </ul>
-           </div>
+            <div class="ft__two">
+                <div class="fl__two__desktop">
+                    <h2>About Us</h2>
+                <ul> 
+                    <li><a href="">Giới thiệu</a></li>
+                    <li><a href="">Lợi ích</a></li>
+                    <li><a href="">Buy BKT</a></li>
+                    <li><a href="">Hướng dẫn</a></li>
+                    <li><a href="">Liện hệ</a></li>
+                </ul>
+                
+                </div>
+                <div class="fl__two__mobi">
+                    <h2>About Us</h2>
+                <ul> 
+                    <li><a href="">Introduce </a></li>
+                    <li><a href="">Benefit </a></li>
+                    <li><a href="">Buy BTR </a></li>
+                    <li><a href="">Tutorial </a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+                
+                </div>
+            </div>
+            <div class="fl__three">
+                <div class="fl__three__desktop">
+                    <h2>Hướng Dẫn</h2>
+                    <ul> 
+                        <li><a href="#home">Rút tiền</a></li>
+                        <li><a href="#news">Nạp tiền</a></li>
+                        <li><a href="#contact">Lấy mã </a></li>
+                        <li><a href="#about">Chuyển tiền</a></li>
+                        <li><a href="#about">Kiểm tra tickit</a></li>
+                    </ul>
+                </div>
+                <div class="fl__three__mobi">
+                    <h2>Hướng Dẫn</h2>
+                    <ul> 
+                        <li><a href="#home">Withdrawal </a></li>
+                        <li><a href="#news">Recharge </a></li>
+                        <li><a href="#contact">Get referral code  </a></li>
+                        <li><a href="#about">Check the ticket </a></li>
+                        <li><a href="#about">Transfers</a></li>
+                    </ul>
+                </div>
+            </div>
        </div>
    </footer>
 </main>
