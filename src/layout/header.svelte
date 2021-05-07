ed91cdbed0dde173c2c0d3ba5aa03181a5bf56c8
hieubui09 <hieubdph09616@fpt.edu.vn>
2021-05-08 00:05:03 +0700

    8-5

--- src/layout/header.svelte
+++ src/layout/header.svelte
@@ -12,16 +12,31 @@
                 <i class="fas fa-globe" />
                 <p>ENG</p>
                 <img class="eng__vector" src="images/Vector.png" alt="" />
+                
             </div>
         </div>
-        <div>
-            <ul class="menu">
+        <div class="menu__all">
+            <div class="menu__mobi">
+                <div>
+                <img class="cate__vector" src="images/category.png" alt="" />
+                </div>
+                <ul class="menu__mobi">
+                    <li><a href="">Giới thiệu</a></li>
+                    <li><a href="">Lợi ích</a></li>
+                    <li><a href="">Buy BKT</a></li>
+                    <li><a href="">Hướng dẫn</a></li>
+                    <li><a href="">Liện hệ</a></li>
+                </ul>
+            </div>
+           <div class="menu__desktop"> 
+            <ul>
                 <li><a href="">Giới thiệu</a></li>
                 <li><a href="">Lợi ích</a></li>
                 <li><a href="">Buy BKT</a></li>
                 <li><a href="">Hướng dẫn</a></li>
                 <li><a href="">Liện hệ</a></li>
             </ul>
+          </div>
         </div>
         <div class="join">
             <div class="join__Now">
